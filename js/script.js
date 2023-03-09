c28f6c5fdab63ac2545238c33abd9e44622634f7
root <root@194-58-118-87.cloudvps.regruhosting.ru>
2023-03-09 23:26:57 +0300

    console...

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
+(function(){
+	document.addEventListener("DOMContentLoaded", function() {
+		document.getElementById("console").innerHTML += "!!! \n||:\n";
+		alert("hoy!!");
+		conPrint(1);
+		console.log("2");
+	});
 
-window.addEventListener('load', function() {
-    console.log('All assets are loaded')
-})
+	function conPrint(msg){
+		document.getElementById("console").innerHTML += msg + "\n";
+	}
+	var _oLog = console.log;
+	console.log = function(message) {
+		conPrint(message);
+		_oLog(message);
+	}
+})();
