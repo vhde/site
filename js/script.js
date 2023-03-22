ae66b1f9dab580c10badd0cff43d64a7bf62011a
root <root@194-67-93-36.cloudvps.regruhosting.ru>
2023-03-22 21:31:41 +0300

    environment headers

--- js/script.js
+++ js/script.js
@@ -1,17 +1,61 @@
 (function(){
-	document.addEventListener("DOMContentLoaded", function() {
+	function onPageLoad() {
 		document.getElementById("console").innerHTML += "!!! \n||:\n";
-		alert("hoy!!");
+		//alert("hoy!!");
 		conPrint(1);
 		console.log("2");
-	});
+		console.log("33");
+	};
+
+	document.addEventListener("DOMContentLoaded", onPageLoad);
+
+	function getHeaders() {
+		console.log("getHeaders...");
+		console.log("... ...");
+		var req = new XMLHttpRequest();
+		console.log("xml...");
+		req.open('GET', "/void.html", false);
+		req.send(null);
+		console.log("request....");
+		var data = new Object();
+		data = {
+				env: ""
+		}
+
+		var headers = req.getAllResponseHeaders().toLowerCase();
+		console.log(headers);
+		var hh1 = headers.split('\n');
+		for(var i=0; i < hh1.length; i++) {
+			var thisItem = hh1[i];
+			var key = thisItem.substring(0, thisItem.indexOf(':'));
+			var value = thisItem.substring(thisItem.indexOf(':')+2, thisItem.length-1);
+			data[key] = value;
+		}
+		console.log(data['env']);
+		
+	}
+
+	document.addEventListener("DOMContentLoaded", getHeaders);
 
 	function conPrint(msg){
-		document.getElementById("console").innerHTML += msg + "\n";
+		var str = document.getElementById("console").innerHTML;
+		var s = str.split("\n");
+		str = msg;
+		document.getElementById("console").innerHTML = 
+			msg + "\n" + document.getElementById("console").innerHTML;
 	}
+
+	var _c = 0;
 	var _oLog = console.log;
 	console.log = function(message) {
-		conPrint(message);
+		conPrint('<span class="c">' + _c + "</span> " + message);
 		_oLog(message);
+		_c++;
 	}
+
+	function ttm() {
+		setTimeout(ttm,10000);
+		console.log("tick");
+	}
+	ttm();
 })();
